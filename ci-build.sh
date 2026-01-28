0bc7fd317a4dbd759618a7ffd651c6a453f7ee63
Junian Triajianto <author@junian.dev>
2026-01-28 10:47:09 +0700

    feat: Change dotnet publish output directory from `output` to `publish` and reorder `DOTNET_VERSION` assignment.

--- ci-build.sh
+++ ci-build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+
+DOTNET_VERSION=10.0
+
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-DOTNET_VERSION=10.0
+
 ./dotnet-install.sh -c $DOTNET_VERSION -InstallDir ./dotnet
 ./dotnet/dotnet --version
 ./dotnet/dotnet publish \
     src/BlazorApps/BlazorApps.csproj \
     -c Release \
-    -o output
+    -o publish
